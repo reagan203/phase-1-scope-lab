59ecc0d274b4c35a91fb4f926b4f15130adf02b4
reagan <reaganm746@gmail.com>
2023-10-07 10:27:48 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+// Write your solution in this!
+  var customerName = 'bob';
+
+  function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+  
+  function setBestCustomer() {
+    bestCustomer = 'not bob';
+  }
+  
+  function overwriteBestCustomer() {
+    bestCustomer = 'new value';
+  }
+  
+  const leastFavoriteCustomer = 'John';
+  
+  function changeLeastFavoriteCustomer() {
+     //Attempt to change the constant
+    leastFavoriteCustomer = 'Bob'; 
+    // This will result in an error
+  }
